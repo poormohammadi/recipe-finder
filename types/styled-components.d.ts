8cb6fb429d357e4d043cacc244023e2bf75ad286
Ali Poormohammadi <poormohammmadi@gmail.com>
2025-11-03 05:00:01 +0100

    fix: styled-components type overrides

--- types/styled-components.d.ts
+++ types/styled-components.d.ts
@@ -2,5 +2,6 @@ import { Theme } from "@/theme/theme";
 import "styled-components/native";
 
 declare module "styled-components/native" {
-  export type DefaultTheme = Theme;
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface DefaultTheme extends Theme {}
 }
